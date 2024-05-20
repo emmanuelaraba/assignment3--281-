24e64f762197d964dae20e96f46018e73366d885
emmanuelaraba <emmanuelaraba77@gmail.com>
2024-05-21 11:19:34 +1200

    added code to add all the countries to the map

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -7,15 +7,28 @@ public class MapEngine {
 
   public MapEngine() {
     // add other code here if you want
-    loadMap(); // keep this mehtod invocation
+    loadMap(); // keep this method invocation
   }
 
-  /** invoked one time only when constracting the MapEngine class. */
+  /** invoked one time only when constructing the MapEngine class. */
   private void loadMap() {
+    RiskMap map = new RiskMap();
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
     // add code here to create your data structures
+    // making new country objects and adding them to the map
+    for (String country : countries) {
+      String[] countryData = country.split("\n");
+      Country newCountry =
+          new Country(countryData[0], countryData[1], Integer.parseInt(countryData[2]));
+      map.addCountry(newCountry);
+    }
 
+    // adding adjacencies to the map
+    for (String adjacency : adjacencies) {
+      String[] adjacencyData = adjacency.split(",");
+      // add code here
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
