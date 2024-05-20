a78e1084bd420b26d3caaeb4ae74c74170a71c7d
emmanuelaraba <emmanuelaraba77@gmail.com>
2024-05-20 14:54:16 +1200

    implemented methods to check equality of countries and generate hashcodes

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -11,10 +11,48 @@ public class Country {
     this.taxFees = taxFees;
   }
 
-  public int HashCode() {
+  /**
+   * method to calculate the hash code of the country
+   *
+   * @return the hash code of the country
+   */
+  @Override
+  public int hashCode() {
     final int prime = 31;
     int result = 1;
     result = prime * result + ((continent == null) ? 0 : continent.hashCode());
     return result;
+  }
+
+  /**
+   * mehtod to check if to countries are equal
+   *
+   * @param obj the object to compare with
+   * @return true if the countries are equal, false otherwise
+   */
+  @Override
+  public boolean equals(Object obj) {
+    // method to check if to countries are equal
+    if (this == obj) {
+      return true;
+    }
+
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+
+    Country other = (Country) obj;
+
+    if (continent == null) {
+      if (other.continent != null) {
+        return false;
+      }
+    } else if (!continent.equals(other.continent)) {
+      return false;
+    }
+    return true;
   }
 }
