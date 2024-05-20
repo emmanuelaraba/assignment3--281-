9d6ad763aead052db60d0713e185ebe457a53db7
emmanuelaraba <emmanuelaraba77@gmail.com>
2024-05-20 15:12:04 +1200

    created methods to add and remove adjancencies and remove countries

--- src/main/java/nz/ac/auckland/se281/RiskMap.java
+++ src/main/java/nz/ac/auckland/se281/RiskMap.java
@@ -12,4 +12,22 @@ public class RiskMap {
   public void addCountry(Country country) {
     map.putIfAbsent(country, new ArrayList<>());
   }
+
+  public void addAdjacency(Country country1, Country country2) {
+    addCountry(country1);
+    addCountry(country2);
+    map.get(country1).add(country2);
+    map.get(country2).add(country1);
+  }
+
+  public void removeCountry(Country country) {
+    map.remove(country);
+    // iterating through the map to remove the country from all the adjacency lists
+    map.forEach((key, value) -> value.remove(country));
+  }
+
+  public void removeAdjacency(Country country1, Country country2) {
+    map.get(country1).remove(country2);
+    map.get(country2).remove(country1);
+  }
 }
